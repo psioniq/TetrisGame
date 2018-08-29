1a6fdf9380fabda0522564b0498a1980ed5cf248
psioniq <crono@c.dk>
2018-08-29 16:49:31 +0200

    Initial commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using ModelLibrary;
+//using TetrisClient;
+
+namespace TetrisClient
+{
+    public class Game : MonoBehaviour
+    {
+        //private MainController clientController;
+        
+        // Use this for initialization
+        IEnumerator Start()
+        {
+            yield return null;
+
+            //clientController = new MainController();
+            //clientController.StartClient();           
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
